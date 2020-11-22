68a876d51ad686870b6c4d83fb08fcf9ab76bcdb
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-11-22 02:03:13 +0000

    1️⃣ Initial Commit

---
+++ src/js/site.js
@@ -0,0 +1,9 @@
+﻿window.addEventListener("load", () => {
+    const fades = $(".fade-2");
+    
+    fades.each((index, elem) => {
+        $(elem).delay(300 * index).animate({
+            opacity: 1
+        }, 1000);
+    });
+});
