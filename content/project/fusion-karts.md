5bfd9b908c08172e5f05cc99759252b865c0eff6
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2022-08-22 19:35:31 +0100

    Ported over rough copy of old portfolio site

---
+++ content/project/fusion-karts.md
@@ -0,0 +1,6 @@
+---
+title: Fusion Karts
+content: This is one of the sites containing information about a project I have worked on or am working on.
+---
+
+# This is a post about Fusion Karts
---
+++ static/js/site.js
@@ -0,0 +1,9 @@
+﻿window.addEventListener("load", () => {
+    const fades = $(".fade-2");
+    
+    fades.each((index, elem) => {
+        $(elem).delay(100 * index).animate({
+            opacity: 1
+        }, 2200);
+    });
+});
