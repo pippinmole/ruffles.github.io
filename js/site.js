cdb2b2f6ddd64c0925b9f99594352b51ea2aa027
pippinmole <pippinmole@users.noreply.github.com>
2022-08-24 02:08:33 +0000

    deploy: 3d74ba179f0737920495aae045aaead2b356c8b3

--- js/site.js
+++
@@ -1,9 +0,0 @@
-﻿window.addEventListener("load", () => {
-    const fades = $(".fade-2");
-    
-    fades.each((index, elem) => {
-        $(elem).delay(100 * index).animate({
-            opacity: 1
-        }, 2200);
-    });
-});
